0227cbb5a638188e03b414f604cd2f26c29f7282
danukaem <danukaem@gmail.com>
2021-07-25 15:57:29 +0530

    item, price, brand, ram chat train success

--- actions/actions.py
+++ actions/actions.py
@@ -7,11 +7,13 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+
+
 #
 # class ActionHelloWorld(Action):
 #
@@ -25,3 +27,30 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message['entities']
+        slots = tracker.get_slot('item')
+        print(entities)
+        print("***********entities***************")
+        print()
+        print("**************slots************")
+        print(slots)
+        print("**************************")
+        item_name = 'hello test action response'
+        item_price = 120000
+        for e in entities:
+            if e['entity'] == "item" and e['value'] == 'hard':
+                item_name = 'there is no hard disk'
+
+        dispatcher.utter_message(text=item_name)
+
+        return [SlotSet("price", item_price)]
